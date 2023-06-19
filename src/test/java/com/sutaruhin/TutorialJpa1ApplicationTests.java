f623830b4aff440753ed6c20e7f1f2804cf09f9f
louis <louis@DESKTOP-2VE7A13>
2023-06-19 12:28:32 +0800

    バージョン管理できるように設定

---
+++ src/test/java/com/sutaruhin/TutorialJpa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sutaruhin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialJpa1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
